f34fda87959100b86f38403ba68b61dd842636c3
Matheus20977 <matheus20977@gmail.com>
2022-10-16 12:36:09 -0300

    added animation to keypress/click

--- index.js
+++ index.js
@@ -8,12 +8,16 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
     makeSound(buttonInnerHTML);
 
+    buttonAnimation(buttonInnerHTML);
+
   });
 }
 
 //detecting keyboard press
 document.addEventListener("keydown", function(event) {
   makeSound(event.key);
+
+  buttonAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -49,4 +53,16 @@ function makeSound(key) {
     default:
       console.log(buttonInnerHTML);
   }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+
+
 }
